49d42f19cb30a62cfa8dd79a89267fd5e2a43748
Philip Schill <philip.schill@gmx.de>
2015-07-16 15:35:29 +0200

    Remove couts from FastMultiply. Removed zero-coefficients from the numbers in xcoeff and ycoeff in FastMultiply. Removed recursion by using FundamentalMultiply instead of calling FastMultiply again.

--- FastMultiplication/FastMultiplication/tests.cpp
+++ FastMultiplication/FastMultiplication/tests.cpp
@@ -5,13 +5,13 @@
 
 int main()
 {
-    static const int B = 10;
+    const int B = 10;
 
     // original:
 //    vector<int> x = { 2,3,1,4,1,7,7,8,9,1};
 //	  vector<int> y = { 8,5,2,1,7,1};
 
-    vector<int> x = {2, 3, 1, 4, 1};
+    vector<int> x = {2, 3, 1, 4, 1, 7};
     int x_s = 1;
     vector<int> y = {8, 5, 2, 1};
     int y_s = 0;
@@ -20,7 +20,7 @@ int main()
     x_b.sgn = x_s;
     BadicRepresentation<B> y_b(y);
     y_b.sgn = y_s;
-    BadicRepresentation<B> z_b = FundamentalMultiply<B>(x_b, y_b);
+    BadicRepresentation<B> z_b = FundamentalMultiply(x_b, y_b);
 
     cout << "X in B-adischer Darstellung: " << x_b << endl;
     cout << "Y in B-adischer Darstellung: " << y_b << endl;
@@ -33,14 +33,10 @@ int main()
     y_new.sgn = y_s;
     cout << "X in 2-adischer Darstellung: " << x_new << endl;
     cout << "Y in 2-adischer Darstellung: " << y_new << endl;
-    cout << "X*Y in 2-adischer Darstellung (FundamentalMultiply): " << FundamentalMultiply<2>(x_new, y_new) << endl;
+    cout << "X*Y in 2-adischer Darstellung (FundamentalMultiply): " << FundamentalMultiply(x_new, y_new) << endl;
     cout << endl;
 
-    int max_x = std::ceil(log(x_new.data.size())/log(2));
-    int max_y = std::ceil(log(y_new.data.size())/log(2));
-    int max = std::max(max_x, max_y);
-
-    BadicRepresentation<2> z_new = FastMultiply<B>(x_b, y_b, max);
+    BadicRepresentation<2> z_new = FastMultiply(x_b, y_b);
     cout << "X*Y in 2-adischer Darstellung (FastMultiply): " << z_new << endl;
 
     std::cout << "done" << std::endl;
